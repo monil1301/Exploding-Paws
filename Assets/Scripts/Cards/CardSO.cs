ee160a9c839a979459b283bdc4214ddddf81b890
monil1301 <mvshah1301@gmail.com>
2024-08-26 01:21:26 +0530

    Moving CardSo to namespace

--- Assets/Scripts/Cards/CardSO.cs
+++ Assets/Scripts/Cards/CardSO.cs
@@ -1,31 +1,35 @@
 using UnityEngine;
+using Utilities;
 
-[CreateAssetMenu(menuName = "Card SO")]
-public class CardSO : ScriptableObject
+namespace Cards
 {
-   public int cardNumber;
-   public CardName cardName;
-   public string cardActionDescription;
-   public Sprite cardIcon;
-   public CardTheme cardTheme;
-   public CardType cardType;
-   public CardRarity cardRarity;
-   public Sprite cardFrame;
-
-   [HideInInspector]
-   public string cardID; // Hidden in inspector to avoid accidentally updating it
-
-   private void GenerateCardID()
+   [CreateAssetMenu(menuName = "Card SO")]
+   public class CardSo : ScriptableObject
    {
-      string themeAbbreviation = CardAbbriviation.GetCardThemeAbbreviations(cardTheme);
-      string nameAbbreviation = CardAbbriviation.GetCardNameAbbreviations(cardName);
+      public int cardNumber;
+      public CardName cardName;
+      public string cardActionDescription;
+      public Sprite cardIcon;
+      public CardTheme cardTheme;
+      public CardType cardType;
+      public CardRarity cardRarity;
+      public Sprite cardFrame;
 
-      cardID = $"{themeAbbreviation}_{nameAbbreviation}_{cardNumber:D3}";
-   }
+      [HideInInspector]
+      public string cardID; // Hidden in inspector to avoid accidentally updating it
 
-   // Automatically generate the card ID when the ScriptableObject is modified
-   private void OnValidate()
-   {
-      GenerateCardID();
+      private void GenerateCardID()
+      {
+         string themeAbbreviation = CardAbbreviation.GetCardThemeAbbreviations(cardTheme);
+         string nameAbbreviation = CardAbbreviation.GetCardNameAbbreviations(cardName);
+
+         cardID = $"{themeAbbreviation}_{nameAbbreviation}_{cardNumber:D3}";
+      }
+
+      // Automatically generate the card ID when the ScriptableObject is modified
+      private void OnValidate()
+      {
+         GenerateCardID();
+      }
    }
 }
